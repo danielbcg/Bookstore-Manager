e023021794fb596ac097f87e65ab2086e228e26e
DanielBonypucminas <danielbony2105@gmail.com>
2026-08-27 16:28:16 -0300

    First commit: initial project structure

---
+++ src/main/java/com/danielbony/bockstoremanager/BockstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.danielbony.bockstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BockstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BockstoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielbony/bockstoremanager/BockstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielbony.bockstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BockstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
